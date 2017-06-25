c24a43761083bbd7b0ab76102ab256e4d4e5f0c6
Michael Corcoran <tracer@outlook.co.nz>
2017-06-25 17:34:01 +1200

    make deps

--- make/winx86/appveyor_build.sh
+++ make/winx86/appveyor_build.sh
@@ -3,4 +3,8 @@ cd "${APPVEYOR_BUILD_FOLDER}"
 
 export IGNORE_MISSING_TOOLCHAIN=yes
 
+make arm_sdk_install
+make openssl_install
+make zip_install
+make breakpad_install
 make -j4 gcs GCS_BUILD_CONF=release
